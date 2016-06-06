a7af3978f648fff8ebf2bd37fc96db7a485260bd
52136 <1024576118@qq.com>
2016-06-07 00:26:17 +0800

    2016/06/07    00:30

--- jdk源码/src/src/java/util/集合源码阅读.java
+++ jdk源码/src/src/java/util/集合源码阅读.java
@@ -5,4 +5,14 @@
 */
 1、AbstractCollection<E>
 此类提供了Collection接口最基础的实现，最大限度地减少了实现此接口所需的工作
-2、
+2、 ArrayDeque<E>
+ public void addFirst(E e) {
+        if (e == null)
+            throw new NullPointerException();
+        elements[head = (head - 1) & (elements.length - 1)] = e;		//防止下标越界或者不合法
+        if (head == tail)
+            doubleCapacity();		//队列已满，则需要进行扩容
+    }
+      private boolean delete(int i)方法没看懂
+      615行
+      
