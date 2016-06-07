1d92d6ec1cb0d9c82adcc27ebded240c31e86e03
52136 <1024576118@qq.com>
2016-06-07 19:25:49 +0800

    2016/06/07  19:24  工具类，使用时再查询

--- jdk源码/src/src/java/util/Calendar.java
+++ jdk源码/src/src/java/util/Calendar.java
@@ -288,7 +288,7 @@ import sun.util.resources.LocaleData;
  * @author Mark Davis, David Goldsmith, Chen-Lieh Huang, Alan Liu
  * @since JDK1.1
  */
-public abstract class Calendar implements Serializable, Cloneable, Comparable<Calendar> {
+public abstract class Calendar implements Serializable, Cloneable, Comparable<Calendar> {	//计算时间
 
     // Data flow in Calendar
     // ---------------------
